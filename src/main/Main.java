89bb6846512a6e71bfbc0f342b493c47fd60c880
TanguyVaucher <1999tanguy@gmail.com>
2025-06-02 12:40:16 +0200

    todo updated

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,10 @@ package main;
 public class Main {
 
     /*
-    1) Castle : Coder la quête finale (Alberto tue après 15 Sec)
-    2) Commentaires + Debug
+    1) Castle : Coder dialogue Alberto (Tue après 10 Sec) --> Win : Ajouer 'orbe' à l'inventaire
+    2) End-Game donner orbe à Maman + Text & Crédits
     3) Devoir 6
+    4) Commentaires + Debug
     */
 
     public static void main(String[] args) {
