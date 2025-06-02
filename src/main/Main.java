43ae7b890d7a9d6d4d1d5924725e4866b5c105d4
TanguyVaucher <1999tanguy@gmail.com>
2025-06-02 16:42:19 +0200

    update todo

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,10 +3,9 @@ package main;
 public class Main {
 
     /*
-    1) End-Game donner orbe à Maman + Text final & Crédits --> Quit
-    2) Cteleport ('teleport <lieu>' (sauf si locked) quand on a l'objet crystal la commande s'ajoute dans help)
-    3) CSave (sauvegarde la progression du joueur) + Permet de rejouer ou recharger la partie précédente (exécute toutes les commandes à la suite)
-    4) Debug (retester les cas interdits) + Commenter le code + Améliorer la fluidité du texte autant que possible
+    1) Cteleport ('teleport <lieu>' (sauf si locked) quand on a l'objet crystal la commande s'ajoute dans help)
+    2) CSave (sauvegarde la progression du joueur) + Permet de rejouer ou recharger la partie précédente (exécute toutes les commandes à la suite)
+    +) Debug (retester les cas interdits) + Commenter le code + Améliorer la fluidité du texte autant que possible
     */
 
     public static void main(String[] args) {
