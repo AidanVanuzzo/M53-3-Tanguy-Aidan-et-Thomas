5ca4b4c74ee54163609219b5c94852a8cb896e69
TanguyVaucher <1999tanguy@gmail.com>
2025-06-02 15:08:02 +0200

    todo updated

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,11 +3,10 @@ package main;
 public class Main {
 
     /*
-    1) Castle : Coder dialogue Alberto (Tue après 10 Sec --> Quit) --> Win : 'orbe' ajouté à l'inventaire
-    2) End-Game donner orbe à Maman + Text final & Crédits --> Quit
-    3) Cteleport ('teleport <lieu>' (sauf si locked) quand on a l'objet crystal la commande s'ajoute dans help)
-    4) CSave (sauvegarde la progression du joueur) + Permet de rejouer ou recharger la partie précédente (exécute toutes les commandes à la suite)
-    5) Debug (tester les cas interdits) + Commenter le code + Améliorer la fluidité du texte autant que possible
+    1) End-Game donner orbe à Maman + Text final & Crédits --> Quit
+    2) Cteleport ('teleport <lieu>' (sauf si locked) quand on a l'objet crystal la commande s'ajoute dans help)
+    3) CSave (sauvegarde la progression du joueur) + Permet de rejouer ou recharger la partie précédente (exécute toutes les commandes à la suite)
+    4) Debug (retester les cas interdits) + Commenter le code + Améliorer la fluidité du texte autant que possible
     */
 
     public static void main(String[] args) {
