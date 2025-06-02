e210e256dc3665ad5e6764a26b2254e9d5642f2c
TanguyVaucher <1999tanguy@gmail.com>
2025-06-02 18:21:01 +0200

    --

--- src/main/CHelp.java
+++ src/main/CHelp.java
@@ -25,7 +25,7 @@ public class CHelp implements ICommand {
         printCommand("look");
         printCommand("say");
         printCommand("inventory");
-        System.out.println(" quit - quits the game\n");
+        System.out.println(" quit - exit the game\n");
     }
 
     private void printCommand(String name) {
