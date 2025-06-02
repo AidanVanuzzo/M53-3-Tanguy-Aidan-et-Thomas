d06282b1b9693a018b907eae91e0986158df4b97
TanguyVaucher <1999tanguy@gmail.com>
2025-06-02 13:25:56 +0200

    -

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
     /*
     1) Castle : Coder dialogue Alberto (Tue après 10 Sec --> Quit) --> Win : 'orbe' ajouté à l'inventaire
     2) End-Game donner orbe à Maman + Text final & Crédits --> Quit
-    3) Devoir 6
+    3) Devoir 6 : CSave (sauvegarde la progression du joueur) + Cteleport ('teleport <lieu>' (sauf si locked) quand on a l'objet crystal la commande s'ajoute dans help)
     4) Commentaires + Debug
     */
 
