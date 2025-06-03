4003a8674d4105133fb80e09f1d8ec1044c8d769
TanguyVaucher <1999tanguy@gmail.com>
2025-06-03 20:45:58 +0200

    comm c registry

--- src/main/CommandRegistry.java
+++ src/main/CommandRegistry.java
@@ -6,14 +6,16 @@ import interfaces.ICommand;
 
 public class CommandRegistry {
 
-    private Map<String, ICommand> baseCommands; // Commandes permanentes
+    //Collection pour stocker les commandes
+    private Map<String, ICommand> baseCommands;
+    //Référence à l'instance actuelle du jeu
     private Game game;
 
+    //Constructeur
     public CommandRegistry(Game game) {
         this.game = game;
         baseCommands = new HashMap<>();
-
-        // Commandes de base
+        //Commandes de niveau 1
         baseCommands.put("help", new CHelp(baseCommands));
         baseCommands.put("map", new CMap(game.getWorldMap()));
         baseCommands.put("move", new CMove(game));
@@ -23,34 +25,40 @@ public class CommandRegistry {
         baseCommands.put("inventory", new CInventory(game));
     }
 
+    //Exécution des commandes tapées par le joueur
     public void commandExecute() {
+        //Entrés clavier
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         String input;
 
         while (true) {
-            // Génère les commandes dynamiques à chaque tour
+            //Génère les commandes à chaque tour
             Map<String, ICommand> currentCommands = new HashMap<>(baseCommands);
 
-            // Si le joueur a le crystal, on ajoute "teleport"
+            //Si le joueur a le crystal, on ajoute 'teleport'
             if (game.getPlayer().getItemByName("crystal") != null) {
                 currentCommands.put("teleport", new CTeleport(game));
             }
 
-            // Met à jour la commande help pour qu’elle affiche les commandes actuelles
+            //Met à jour la commande 'help' pour qu’elle affiche les commandes actuelles
             currentCommands.put("help", new CHelp(currentCommands));
 
+            //Champ de saisie des commandes
             System.out.print("Enter a command : ");
             input = scanner.nextLine().toLowerCase();
 
+            //Permet de quitter la partie
             if (input.equals("quit")) {
                 System.out.println("\n[See you next time.]\n");
                 break;
             }
 
+            //Cherche et lance la commande
             ICommand command = currentCommands.get(input);
             if (command != null) {
                 command.execute();
             } else {
+                //Rappel de 'help' en cas de mauvaise saisie
                 System.out.println("Invalid command. Try 'help'.");
             }
         }
