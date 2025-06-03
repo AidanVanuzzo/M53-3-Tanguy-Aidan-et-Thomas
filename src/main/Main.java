221a0f5b076c6c44fa034dad8f5a53d3b268e317
TanguyVaucher <1999tanguy@gmail.com>
2025-06-03 09:41:41 +0200

    --

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,16 +8,15 @@ public class Main {
     +) Debug (tester le jeu et tous cas interdits)
     +) Commenter tout le code du main
     +) Améliorer la fluidité du texte de jeu autant que possible
+    --> Rendu Rapport
     */
 
     public static void main(String[] args) {
     Game game = new Game();
     Scanner scanner = new Scanner(System.in);
-
     System.out.print("\n\nDo you want to load the last save or start a new game? (load/new): \n\n");
     String choice = scanner.nextLine().trim().toLowerCase();
     
-    //ici
 
     if (choice.equals("load")) {
         game.setLoadRequested(true);
