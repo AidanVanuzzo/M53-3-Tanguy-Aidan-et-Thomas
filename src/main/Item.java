06c05c4b865c8ca64c5fc099420edec0bdcebb64
TanguyVaucher <1999tanguy@gmail.com>
2025-06-03 20:31:42 +0200

    Comm game

--- src/main/Item.java
+++ src/main/Item.java
@@ -2,16 +2,22 @@ package main;
 
 public class Item {
 
+    //Les Items comprennent : clés, PNJ et distracteurs (inutiles, mais on peut les inspecter)
+    
+    //En fonction du besoin, description et useZone (position de l'objet sur la carte) peuvent être null
     private String name;
     private String description;
     private String useZone;
 
+    //Constructeur
     public Item(String name, String description, String useZone) {
         this.name = name;
         this.description = description;
         this.useZone = useZone;
     }
 
+    //Getters pour les trois attributs
+    
     public String getName() {
         return name;
     }
